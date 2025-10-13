0bb870150cc1a79a8d5a2cd16b59e7bd3082acf6
JiriStipek <567776@muni.cz>
2025-10-13 22:19:04 +0200

    feat: add logger (Serilog)

---
+++ Logger.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Logger.cs
+ * Used external library: Serilog (https://github.com/serilog/serilog
+ * Author: Jiri Stipek
+ * Veeam test task
+ * Configure Serilog for logging to console and file
+ */
+using Serilog;
+
+namespace Veeam_test_task
+{
+    public class Logger
+    {
+        /// <summary>
+        /// Configure Serilog to log to console and to a file with daily rolling and size limit
+        /// </summary>
+        /// <param name="filePath"></param>
+        public static void Configure(string filePath)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File(Path.Combine(filePath, "log.txt"),
+                    rollingInterval: RollingInterval.Day,
+                    rollOnFileSizeLimit: true)
+                .CreateLogger();
+        }
+    }
+}
