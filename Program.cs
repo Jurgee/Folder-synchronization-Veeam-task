5c7a45d54227a9d8a5eac37f29807552df2f673d
JiriStipek <567776@muni.cz>
2025-10-14 22:31:49 +0200

    docs: add header

--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿using Serilog;
+﻿/*
+ * Program.cs
+ * Author: Jiri Stipek
+ * Veeam test task
+ * Main program class for Veeam test task
+ */
+using Serilog;
 
 namespace Veeam_test_task
 {
     internal class Program
     {
-
         static void Main(string[] args)
         {
             var parser = new ArgumentParser();
@@ -23,18 +28,14 @@ namespace Veeam_test_task
 
                 // Validate log
                 Validator.ValidatePath(result.LogFile, Validator.PathType.Log);
-                Log.Information("Log path validated at {LogPath}", result.LogFile);
 
                 // Validate interval
                 Validator.ValidateInterval(result.Interval);
-                Log.Information("Interval validated at {Interval} seconds", result.Interval);
 
                 // Validate source and backup paths
                 Validator.PathsAreSameOrNested(result.SourceFolder, result.BackupFolder);
                 Validator.ValidatePath(result.SourceFolder, Validator.PathType.Source);
-                Log.Information("Source path validated at {SourcePath}", result.SourceFolder);
                 Validator.ValidatePath(result.BackupFolder, Validator.PathType.Backup);
-                Log.Information("Backup path validated at {BackupPath}", result.BackupFolder);
 
                 // Start synchronization
                 syncTimer = Timer.SetTimer(result.Interval, result.SourceFolder, result.BackupFolder);
